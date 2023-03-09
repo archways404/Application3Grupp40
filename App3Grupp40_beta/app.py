cd71516c538b8a7f3cab48928bcd8e9561ae1094
archways <107893165+archways404@users.noreply.github.com>
2023-03-09 18:30:34 +0100

    Major upgrade/update

--- App3Grupp40_beta/app.py
+++ App3Grupp40_beta/app.py
@@ -1,4 +1,5 @@
-#app.py
+#Beta v2.0 *(9/3)
+
 from flask import Flask, request, session, redirect, url_for, render_template, flash
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
@@ -6,119 +7,93 @@ import re
 from werkzeug.security import generate_password_hash, check_password_hash
  
 app = Flask(__name__)
-app.secret_key = 'cairocoders-ednalan'
+app.secret_key = 'grupp40'
  
 DB_HOST = "pgserver.mau.se"
 DB_NAME = "ak8893_test"
 DB_USER = "ak8893"
-DB_PASS = "XXXXXXXXXXXXXXX" # Pass exists in your discord
+DB_PASS = "xrbz4n8h"
  
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
  
 @app.route('/')
 def home():
-    # Check if user is loggedin
     if 'loggedin' in session:
-    
-        # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'])
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
- 
+        if session['is_admin'] == True:
+            return render_template('home.html', email=session['email'], is_admin=session['is_admin'] )
+        else:
+            return render_template('home.html', email=session['email'], is_admin=session['is_admin']) #REMOVE AFTER TESTING
+    return render_template('home.html')
+
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-   
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
         password = request.form['password']
         print(password)
- 
-        # Check if account exists using MySQL
-        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
-        # Fetch one record and return result
+        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
         account = cursor.fetchone()
- 
         if account:
             password_rs = account['password']
             print(password_rs)
-            # If account exists in users table in out database
             if check_password_hash(password_rs, password):
-                # Create session data, we can access this data in other routes
                 session['loggedin'] = True
                 session['id'] = account['id']
-                session['username'] = account['username']
-                # Redirect to home page
+                session['email'] = account['email']
+                session['is_admin'] = account['is_admin']
                 return redirect(url_for('home'))
             else:
-                # Account doesnt exist or username/password incorrect
                 flash('Incorrect username/password')
         else:
-            # Account doesnt exist or username/password incorrect
             flash('Incorrect username/password')
- 
     return render_template('login.html')
   
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
- 
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
-        fullname = request.form['fullname']
-        username = request.form['username']
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        first_name = request.form['fullname']
+        last_name = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        is_admin = False
     
         _hashed_password = generate_password_hash(password)
  
-        #Check if account exists using MySQL
-        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
         account = cursor.fetchone()
         print(account)
-        # If account exists show error and validation checks
         if account:
             flash('Account already exists!')
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             flash('Invalid email address!')
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            flash('Username must contain only characters and numbers!')
-        elif not username or not password or not email:
+        elif not password or not email:
             flash('Please fill out the form!')
         else:
-            # Account doesnt exists and the form data is valid, now insert new account into users table
-            cursor.execute("INSERT INTO users (fullname, username, password, email) VALUES (%s,%s,%s,%s)", (fullname, username, _hashed_password, email))
+            cursor.execute("INSERT INTO users (first_name, last_name, password, email, is_admin) VALUES (%s,%s,%s,%s,%s)", (first_name, last_name, _hashed_password, email, is_admin))
             conn.commit()
             flash('You have successfully registered!')
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
         flash('Please fill out the form!')
-    # Show registration form with message (if any)
     return render_template('register.html')
    
    
 @app.route('/logout')
 def logout():
-    # Remove session data, this will log the user out
    session.pop('loggedin', None)
    session.pop('id', None)
-   session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
+   session.pop('email', None)
+   session.pop('is_admin', None)
+   return redirect(url_for('home'))
   
 @app.route('/profile')
 def profile(): 
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-   
-    # Check if user is loggedin
     if 'loggedin' in session:
         cursor.execute('SELECT * FROM users WHERE id = %s', [session['id']])
         account = cursor.fetchone()
-        # Show the profile page with account info
         return render_template('profile.html', account=account)
-    # User is not loggedin redirect to login page
     return redirect(url_for('login'))
  
 if __name__ == "__main__":
